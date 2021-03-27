e3ba4e3689f9d450c3c57e8b41b83b6d66d8c221
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-03-26 17:59:07 -0700

    Initial Commit

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    public int ageToMate;
+    public int ageToDie;
+
+    private Vector2 currentPos;
+    private Environment environment;
+    private int currentAge;
+
+    public Creature(Environment envir)
+    {
+        environment = envir;
+        currentAge = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pickDirectionToMoveIn()
+    {
+        
+    }
+}
